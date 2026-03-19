b23449437f03ebbe01ef03a0afc25aeb7030d493
DianaEst0069 <ecdo230069@upemor.edu.mx>
2026-03-19 15:59:40 -0600

    Version 2

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -6,6 +6,8 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use Illuminate\Support\Facades\Auth;
+
 class AdminMiddleware
 {
     /**
@@ -15,6 +17,18 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        //Validar si el usuario tiene sesión activa
+        if(!Auth::check()){
+            return redirect()->route('registro')
+            ->with('error', 'Se debe registrar e iniciar sesión');
+        }
+
+        //Validar si el usuario actual es administrador
+        if(!Auth::user()->is_admin){
+            return redirect()->route('libros.index')
+            ->with('error', 'No cuentas con permisos de administrador');
+        }
+
         return $next($request);
     }
 }
